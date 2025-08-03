2a66978768ea907139d43bbd4060f60c67e81845
topfacemedia <bm@topfacemedia.com>
2025-08-03 17:25:41 +0300

    change tg auth logic onto session string

--- main.py
+++ main.py
@@ -4,16 +4,47 @@ from datetime import datetime
 from openpyxl import Workbook
 from openpyxl.styles import Alignment
 import tempfile
+from contextlib import asynccontextmanager
 
 from .services.data_collect import collect_uni_stats
 from .services.parsers.mratingparser import get_unis
 
 from .models.schemas import AnalyzeRequest
 
+from pyrogram import Client
+import os
+from dotenv import load_dotenv
 
-app = FastAPI()
+load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await startup()
+    yield
+    await shutdown()
+
+app = FastAPI(lifespan=lifespan)
 router = APIRouter()
 
+tg_api_id = os.environ.get("TG_API_ID")
+tg_api_hash = os.environ.get("TG_API_HASH")
+tg_session_name = os.environ.get("TG_SESSION_NAME")
+tg_session_string = os.environ.get("TG_SESSION_STRING")
+
+async def startup():
+    global app_client
+    app_client = Client(
+        name="my_userbot",
+        api_id=tg_api_id,
+        api_hash=tg_api_hash,
+        session_string=tg_session_string,
+        in_memory=True
+    )
+    await app_client.start()
+
+async def shutdown():
+    global app_client
+    await app_client.stop()
 
 @app.get("/")
 def read_root():
@@ -26,7 +57,7 @@ async def analyze_endpoint(data: AnalyzeRequest):
     results = {}
     for webname in universities:
         results[webname] = await collect_uni_stats(
-            webname, data.start_date, data.end_date, data.socials
+            webname, data.start_date, data.end_date, data.socials, app_client
         )
 
     return results
--- services/data_collect.py
+++ services/data_collect.py
@@ -19,9 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 vk_access_token = os.environ.get("VK_ACCESS_TOKEN")
-tg_api_id = os.environ.get("TG_API_ID")
-tg_api_hash = os.environ.get("TG_API_HASH")
-tg_session_name = os.environ.get("TG_SESSION_NAME")
+
 
 def get_month_range(start_date: date, end_date: date, year: int) -> Tuple[int, int]:
     if year == start_date.year == end_date.year:
@@ -51,87 +49,94 @@ def get_period_dates(
     return start_dt, end_dt
 
 async def collect_uni_stats(
-    webname: str, start_date: date, end_date: date, socials: List[str]
+    webname: str, start_date: date, end_date: date, socials: List[str], app: Client
 ) -> dict:
     if not webname:
         return {"result": "error", "message": "Webname is required."}
     data = list()
-    async with Client(tg_session_name, api_id=tg_api_id, api_hash=tg_api_hash) as app:
-        uni_info = get_socials(webname)
-        if not uni_info:
-            return {
-                "result": "error",
-                "message": "University not found or no social media data available.",
-            }
+    # async with Client(tg_session_name, api_id=tg_api_id, api_hash=tg_api_hash, no_updates=True, in_memory=True) as app:
+    uni_info = get_socials(webname)
+    if not uni_info:
+        return {
+            "result": "error",
+            "message": "University not found or no social media data available.",
+        }
+    try:
         uni_short_name = uni_info["shortName"]
         uni_id = uni_info["id"]
         uni_vk = uni_info["vk"]
         uni_tg = uni_info["tg"]
         uni_rutube = uni_info["rutube"]
-        rutube_stats_dict = (
-            rutube_posts_and_views_parser(
-                datetime(start_date.year, start_date.month, start_date.day, 23, 59, 59),
-                uni_rutube
-            )
-            if uni_rutube and "rutube" in socials
-            else {}
+    except KeyError:
+        print(f"Invalid university data format for {webname}.")
+        return {
+            "result": "error",
+            "message": "Invalid university data format.",
+        }
+    rutube_stats_dict = (
+        rutube_posts_and_views_parser(
+            datetime(start_date.year, start_date.month, start_date.day, 23, 59, 59),
+            uni_rutube
         )
-        for year in range(start_date.year, end_date.year + 1):
-            mrate = get_rating_data(uni_id, year)
-            month_start, month_end = get_month_range(start_date, end_date, year)
-            for month in range(month_start, month_end + 1):
-                start_dt, end_dt = get_period_dates(year, month, start_date, end_date)
-                rutube_posts, rutube_views = (
-                    (rutube_stats_dict.get(start_dt.strftime("%m.%Y"), (0, 0)))
-                    if "rutube" in socials
-                    else (0, 0)
-                )
-                posts_tg, views_tg, reacts_tg, forwards_tg = (
-                    await tg_ultimate_parser(uni_tg, start_dt, end_dt, app)
-                    if uni_tg and "tg" in socials
-                    else (0, 0, 0, 0)
-                )
-                comments_tg = 0 #(
-                    # await tg_comments_counter(uni_tg, start_dt, end_dt, app)
-                    # if uni_tg and "tg" in socials
-                    # else 0
-                # )
-                (
-                    mrate_cons,
-                    mrate_socials,
-                    mrate_vk,
-                    mrate_tg,
-                    mrate_rutube,
-                    mrate_site,
-                    mrate_smi,
-                ) = mrate[month - 1]
-                posts_vk, views_vk, reacts_vk, reposts_vk, comments_vk = (
-                    (vk_ultimate_parser(uni_vk, start_dt, end_dt, vk_access_token))
-                    if uni_vk and "vk" in socials
-                    else (0, 0, 0, 0, 0)
-                )
-                data.append(
-                    {
-                        "month": months_rus.get(month),
-                        "mrating": mrate_cons,
-                        "mrating_socials": mrate_socials,
-                        "mrating_vk": mrate_vk,
-                        "mrating_tg": mrate_tg,
-                        "mrating_rutube": mrate_rutube,
-                        "mrating_site": mrate_site,
-                        "mrating_smi": mrate_smi,
-                        "posts_vk": posts_vk,
-                        "views_vk": views_vk,
-                        "reacts_vk": reacts_vk,
-                        "reposts_vk": reposts_vk,
-                        "comments_vk": comments_vk,
-                        "posts_tg": posts_tg,
-                        "views_tg": views_tg,
-                        "reacts_tg": reacts_tg,
-                        "forwards_tg": forwards_tg,
-                        "comments_tg": comments_tg,
-                        "posts_rutube": rutube_posts,
-                        "views_rutube": rutube_views,
-                    }
-                )
+        if uni_rutube and "rutube" in socials
+        else {}
+    )
+    for year in range(start_date.year, end_date.year + 1):
+        mrate = get_rating_data(uni_id, year)
+        month_start, month_end = get_month_range(start_date, end_date, year)
+        for month in range(month_start, month_end + 1):
+            start_dt, end_dt = get_period_dates(year, month, start_date, end_date)
+            rutube_posts, rutube_views = (
+                (rutube_stats_dict.get(start_dt.strftime("%m.%Y"), (0, 0)))
+                if "rutube" in socials
+                else (0, 0)
+            )
+            posts_tg, views_tg, reacts_tg, forwards_tg = (
+                await tg_ultimate_parser(uni_tg, start_dt, end_dt, app)
+                if uni_tg and "tg" in socials
+                else (0, 0, 0, 0)
+            )
+            comments_tg = (
+                await tg_comments_counter(uni_tg, start_dt, end_dt, app)
+                if uni_tg and "tg" in socials
+                else 0
+            )
+            (
+                mrate_cons,
+                mrate_socials,
+                mrate_vk,
+                mrate_tg,
+                mrate_rutube,
+                mrate_site,
+                mrate_smi,
+            ) = mrate[month - 1]
+            posts_vk, views_vk, reacts_vk, reposts_vk, comments_vk = (
+                (vk_ultimate_parser(uni_vk, start_dt, end_dt, vk_access_token))
+                if uni_vk and "vk" in socials
+                else (0, 0, 0, 0, 0)
+            )
+            data.append(
+                {
+                    "month": months_rus.get(month),
+                    "mrating": mrate_cons,
+                    "mrating_socials": mrate_socials,
+                    "mrating_vk": mrate_vk,
+                    "mrating_tg": mrate_tg,
+                    "mrating_rutube": mrate_rutube,
+                    "mrating_site": mrate_site,
+                    "mrating_smi": mrate_smi,
+                    "posts_vk": posts_vk,
+                    "views_vk": views_vk,
+                    "reacts_vk": reacts_vk,
+                    "reposts_vk": reposts_vk,
+                    "comments_vk": comments_vk,
+                    "posts_tg": posts_tg,
+                    "views_tg": views_tg,
+                    "reacts_tg": reacts_tg,
+                    "forwards_tg": forwards_tg,
+                    "comments_tg": comments_tg,
+                    "posts_rutube": rutube_posts,
+                    "views_rutube": rutube_views,
+                }
+            )
     return {"university": uni_short_name, "data": data}
